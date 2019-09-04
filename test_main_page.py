2a095d28f67607dc1abf31b8b263223082d102d2
LynxieBoo <rysitsa@yandex.ru>
2019-09-05 00:08:17 +0300

    Add tests for elements of login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,9 @@
+import pytest
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -8,8 +11,16 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_main_elements_is_on_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
