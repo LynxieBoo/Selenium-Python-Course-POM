0970f070942c1c847f30a4c7dff4aa053551e7d6
LynxieBoo <rysitsa@yandex.ru>
2019-09-05 23:09:29 +0300

    Add test for new year action

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.XPATH, '//button[contains(@class, "basket")]')
+    PRODUCT_NAME_ALERT = (By.XPATH, '//div[@class="alertinner " ]/strong[1]')
+    TOTAL_PRICE_ALERT = (By.XPATH, '//div[@class="alertinner "]/p[contains(text(), "basket")]')
+    TOTAL_PRICE_BASKET_MINI = (By.XPATH, '//div[contains(@class,"basket-mini")]')
+    PRODUCT_PRICE = (By.XPATH, '//div[contains(@class,"product_main")]/p[@class="price_color"]')
+    PRODUCT_NAME = (By.XPATH, '//div[contains(@class,"product_main")]/h1')
---
+++ pages/product_page.py
@@ -0,0 +1,68 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        self.should_be_promo_url()
+        self.should_be_add_to_cart_button()
+        self.click_add_product_to_cart_button()
+        self.solve_quiz_and_get_code()
+        self.should_be_same_product_names()
+        self.should_be_same_cart_and_product_cost()
+
+    def should_be_promo_url(self):
+        assert 'promo=newYear' in self.browser.current_url, "'promo=newYear' is not presented in URL"
+
+    def should_be_add_to_cart_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_CART_BUTTON), "'Add to cart' button is not presented"
+
+    def click_add_product_to_cart_button(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON).click()
+
+    def should_be_same_product_names(self):
+        self.should_be_alert_product_name()
+        self.should_be_main_product_name()
+        self.check_same_product_names()
+
+    def should_be_same_cart_and_product_cost(self):
+        self.should_be_main_product_price()
+        self.should_be_mini_cart_cost()
+        self.should_be_alert_cart_cost()
+        self.check_same_cart_and_product_cost()
+
+    def should_be_alert_product_name(self):
+        warning = "Alert with product name is not presented"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME_ALERT), warning
+
+    def should_be_main_product_name(self):
+        warning = "Main product name is not presented"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), warning
+
+    def check_same_product_names(self):
+        main_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        alert_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ALERT).text
+        warning = 'Product name in the cart and on the main field are different'
+        assert main_product_name == alert_product_name, warning
+
+    def should_be_main_product_price(self):
+        warning = "Main product price is not presented"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), warning
+
+    def should_be_mini_cart_cost(self):
+        warning = "Mini cart cost is not presented"
+        assert self.is_element_present(*ProductPageLocators.TOTAL_PRICE_BASKET_MINI), warning
+
+    def should_be_alert_cart_cost(self):
+        warning = "Alert cart cost is not presented"
+        assert self.is_element_present(*ProductPageLocators.TOTAL_PRICE_ALERT), warning
+
+    def check_same_cart_and_product_cost(self):
+        main_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        alert_cart_cost = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_ALERT).text
+        alert_cart_cost = alert_cart_cost.split(' ')[-1]
+        mini_cart_cost = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_BASKET_MINI).text
+        mini_cart_cost = mini_cart_cost.split('\n')[0].split(' ')[2]
+
+        warning = 'Product price, cost in the mini cart and in the alert field are different'
+        assert main_product_price == alert_cart_cost == mini_cart_cost, warning
